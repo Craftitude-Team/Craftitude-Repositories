f599925791f1ae662dd3030bacd3ad974b1db14e
Carl Kittelberger <icedream@blazing.de>
2013-04-09 13:52:26 +0200

    Generated metadata for all versions of the minecraft client which are available from http://assets.minecraft.net/.

---
+++ minecraft-testing/packages/1.4.2/minecraft-client/1.4.2/all/instructions.lua
@@ -0,0 +1,20 @@
+
+function Source()
+	var download = GetResolver("download", {
+		url = "http://assets.minecraft.net/1_4_2/minecraft.jar"
+	}):ResolveToStream()
+	
+	var archive = GetResolver("archive", {
+		input = download
+	}):ResolveToArchive()
+	
+	return archive
+end
+	
+function Install()
+	InjectAllFromSource("bin/minecraft.jar")
+end
+
+function Uninstall()
+	DeleteFile("bin/minecraft.jar")
+end	
---
+++ minecraft-testing/packages/13w09c/minecraft-server/13w09c/all/instructions.lua
@@ -0,0 +1,20 @@
+
+function Source()
+	var download = GetResolver("download", {
+		url = "http://assets.minecraft.net/13w09c/minecraft_server.jar"
+	}):ResolveToStream()
+	
+	var archive = GetResolver("archive", {
+		input = download
+	}):ResolveToArchive()
+	
+	return archive
+end
+	
+function Install()
+	InjectAllFromSource("bin/minecraft_server.jar")
+end
+
+function Uninstall()
+	DeleteFile("bin/minecraft_server.jar")
+end	

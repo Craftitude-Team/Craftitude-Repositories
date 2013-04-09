7e1c33bc560314da5f3102040f81e5592a51feca
Icedream <icedream2k9@die-optimisten.net>
2013-04-09 12:14:24 +0200

    Add libraries-testing repository.
    + Added package "lwjgl"
    + Added package "lwjgl_util"
    + Added package "lwjgl-platform"
    + Added package "jinput"
    + Added package "jinput-platform"

---
+++ libraries-testing/all/packages/lwjgl-platform/2.8.1/osx/instructions.lua
@@ -0,0 +1,20 @@
+
+function Source()
+	var download = GetResolver("download", {
+		url = "http://repo1.maven.org/maven2/org/lwjgl/lwjgl/lwjgl-platform/2.8.1/lwjgl-platform-2.8.1-natives-osx.jar"
+	}):ResolveToStream()
+	
+	var archive = GetResolver("archive", {
+		input = download
+	}):ResolveToArchive()
+	
+	return archive
+end
+	
+function Install()
+	UnpackAllFromSource("lib/lwjgl")
+end
+
+function Uninstall()
+	DeleteFolder("lib/lwjgl")
+end	
---
+++ libraries-testing/all/packages/lwjgl_util/2.8.2/all/instructions.lua
@@ -0,0 +1,20 @@
+
+function Source()
+	var download = GetResolver("download", {
+		url = "http://repo1.maven.org/maven2/org/lwjgl/lwjgl/lwjgl_util/2.8.2/lwjgl_util-2.8.2-natives-all.jar"
+	}):ResolveToStream()
+	
+	var archive = GetResolver("archive", {
+		input = download
+	}):ResolveToArchive()
+	
+	return archive
+end
+	
+function Install()
+	InjectAllFromSource("lib/lwjgl_util.jar")
+end
+
+function Uninstall()
+	DeleteFile("lib/lwjgl_util.jar")
+end	

e86b43e0ca42a35da22b2436a9f373851bc18626
Craftitude Automatic Repository System <craftitude@modernminas.de>
2013-04-14 17:46:18 +0200

    Fixed libraries package instruction files.

--- libraries-testing/all/packages/lwjgl/2.8.1/instructions.lua
+++ libraries-testing/all/packages/lwjgl/2.8.1/instructions.lua
@@ -4,9 +4,9 @@ function Install()
 		input = GetResolver("download", {
 			url = "http://repo1.maven.org/maven2/org/lwjgl/lwjgl/lwjgl/2.8.1/lwjgl-2.8.1.jar"
 		})
-	}):InjectAllTo("lib/lwjgl.jar/lwjgl.jar")
+	}):InjectAllTo("lib/lwjgl.jar")
 end
 
 function Uninstall()
-	DeleteFile("lib/lwjgl.jar/lwjgl.jar")
+	DeleteFile("lib/lwjgl.jar")
 end
--- libraries-testing/all/packages/lwjgl_util/2.8.2/instructions.lua
+++ libraries-testing/all/packages/lwjgl_util/2.8.2/instructions.lua
@@ -4,9 +4,9 @@ function Install()
 		input = GetResolver("download", {
 			url = "http://repo1.maven.org/maven2/org/lwjgl/lwjgl/lwjgl_util/2.8.2/lwjgl_util-2.8.2.jar"
 		})
-	}):InjectAllTo("lib/lwjgl_util.jar/lwjgl_util.jar")
+	}):InjectAllTo("lib/lwjgl_util.jar")
 end
 
 function Uninstall()
-	DeleteFile("lib/lwjgl_util.jar/lwjgl_util.jar")
+	DeleteFile("lib/lwjgl_util.jar")
 end
